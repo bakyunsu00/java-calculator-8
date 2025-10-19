5806c62d2ac310a0f8a6e84acb815e970f9af50a
박현수 <zgustn0@gmail.com>
2025-10-20 02:34:04 +0900

    feat(SeparatorSumCalculator): 계산 기능 추가 & 공백 예외 추가 & 변수명 변경 - cal(): 공백시 0 반환 추가, 구분자로 나눈 후 합 반환 - 변수명 변경: defaultSeparator -> 상수 DEFAULT_SEPARATOR 명시 - checkCustom -> checkCustomSeparator 정확한 이름으로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        int result = 0;
         SeparatorSumCalculator ssc = new SeparatorSumCalculator();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        ssc.checkCustom(input);
+        ssc.checkCustomSeparator(input);
+        result = ssc.cal();
+        System.out.println("결과 : "+result);
     }
 }

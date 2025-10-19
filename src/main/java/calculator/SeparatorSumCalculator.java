5b2fc3c1e0cd487482872e8feb5709f747b9e4ae
박현수 <zgustn0@gmail.com>
2025-10-20 01:01:43 +0900

    feat(SeparatorSumCalculator): 커스텀 구분자 유무확인 기능 추가
    - checkCustom: 커스텀 구분자 확인 후 구분자 종류에 추가

---
+++ src/main/java/calculator/SeparatorSumCalculator.java
@@ -0,0 +1,50 @@
+package calculator;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SeparatorSumCalculator {
+
+    List<String> inputStrings = new ArrayList<>();
+    String stringWithSeparator;
+
+
+
+
+
+    String defaultSeparator = ",:";
+    String customSeparator;
+    boolean haveCustomSeparator;
+
+
+
+
+
+
+    //커스텀 구분자 유무
+    void checkCustom(String input){
+        for (String str : input.replace("\\n", "\n").replace("//","").split("\n")) {
+            inputStrings.add(str);
+        }
+
+
+        if (inputStrings.size() == 2) {
+            customSeparator = defaultSeparator + inputStrings.get(0);
+            stringWithSeparator = inputStrings.get(1);
+            haveCustomSeparator = true;
+
+        }
+        else{
+            stringWithSeparator = inputStrings.get(0);
+
+        }
+    }
+
+    //계산 실행
+
+
+
+
+}
